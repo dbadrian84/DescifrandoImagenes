b638d15eaecdbe9a0f16e095338608e1ad102015
Dbadrian84 <adriandelgadobox2003@gmail.com>
2024-12-05 20:53:19 +0100

    He copiao el codigo de los apuntes esta vez de verdad

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,27 @@
 package org.example
 
+import javax.crypto.Cipher
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
+
 fun main() {
-    println("Hello World!")
+    val iv = encryptedData.take(16).toByteArray()
+
+    val cipher = Cipher.getInstance("AES/CBC/NoPadding")
+    cipher.init(
+        Cipher.DECRYPT_MODE,
+        SecretKeySpec(key128, "AES"),
+        IvParameterSpec(iv)
+    )
+    val decrypted = cipher.doFinal(encryptedData).drop(16).toByteArray()
+
+    fun expandKey(keyHex: Int): ByteArray {
+        // Convierte el Int hexadecimal a un arreglo de bytes
+        val keyBytes = ByteArray(2)
+        keyBytes[0] = (keyHex shr 8 and 0xff).toByte() // Extrae el primer byte
+        keyBytes[1] = (keyHex and 0xff).toByte() // Extrae el segundo byte
+        // Expande los bytes a 16 bytes
+        return ByteArray(16) { keyBytes[it % keyBytes.size] }
+    }
+
 }
